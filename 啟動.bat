a66ea669ea134b3490fc523ba5f3e5755ea7774e
StyleA <sino0914@gmail.com>
2025-10-21 09:51:18 +0800

    更新使用pnpm i 跟啟動的時候使用到的程式碼相關

--- 啟動.bat
+++ 啟動.bat
@@ -1,7 +1,7 @@
 @echo off
 echo 啟動三個專案...
 
-start "API伺服器" cmd /k "cd /d %~dp0 && cd gwgw-gift-admin\server && npm start"
+start "API伺服器" cmd /k "cd /d %~dp0 && cd packages\admin-app\server && npm start"
 start "前後台伺服器" cmd /k "cd /d %~dp0 && pnpm dev:all"
 
 echo 全部啟動完成！
